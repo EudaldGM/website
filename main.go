aa8c1f08819519ae6cd29eca3f755b5a1c0e78f7
EudaldGM <eudaldguillen@gmail.com>
2024-12-24 21:52:53 +0100

    fix

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	http.HandleFunc("/", home)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
